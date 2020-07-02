91bffd7f6c823db0cae419a5d16989e305030a38
Peter Lewis <820394+peterlewis@users.noreply.github.com>
2020-07-02 19:48:15 +0100

    Create font-cascadia-pl-otf.rb

---
+++ Casks/font-cascadia-pl-otf.rb
@@ -0,0 +1,16 @@
+cask 'font-cascadia-pl-otf' do
+  version '2007.01'
+  sha256 '9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4'
+
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
+  appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
+  name 'Cascadia PL'
+  homepage 'https://github.com/microsoft/cascadia-code'
+
+  font 'otf/CascadiaCodePL-Bold.otf'
+  font 'otf/CascadiaCodePL-ExtraLight.otf'
+  font 'otf/CascadiaCodePL-Light.otf'
+  font 'otf/CascadiaCodePL-Regular.otf'
+  font 'otf/CascadiaCodePL-SemiBold.otf'
+  font 'otf/CascadiaCodePL-SemiLight.otf'
+end
