1f701961bdac1b90fb955b9adae114ae2f025f6f
Peter Lewis <peter@peterlew.is>
2020-08-08 10:35:28 +0100

    Create wickrpro.rb

---
+++ Casks/wickrpro.rb
@@ -0,0 +1,12 @@
+cask "wickrpro" do
+  version "5.60.16"
+  sha256 "fe4bd049dce0538ce67d8494b75bf053d0ff76855488c68964f3768e227a4a21"
+
+  # s3.amazonaws.com/static.wickr.com/ was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/pro/WickrPro-#{version}.dmg"
+  appcast "https://pro-download.wickr.com/api/multiVerify/pro/undefined/"
+  name "Wickr Pro"
+  homepage "https://wickr.com/products/pro/"
+
+  app "WickrPro.app"
+end
