eb17d3bdb1640758820fc733e42cf4dd5435979b
Peter Lewis <peter@peterlew.is>
2020-08-15 11:24:59 +0100

    Update minelab-update-utility.rb

--- Casks/minelab-update-utility.rb
+++ Casks/minelab-update-utility.rb
@@ -1,6 +1,6 @@
 cask "minelab-update-utility" do
   version "3.0"
-  sha256 "1f50668d58ffed36a658d4f3c50feb9ed0d06ce7db20c77a3bc9031c2fb02b69"
+  sha256 "d65c6adb89737b202f30a117b3ba2f8a4d95415e7352a6b0c9efe1f4e4f6b599"
 
   url "https://www.minelab.com/__files/d/434812/Minelab%20Update%20Utility%20-%20EQUINOX%20Upgrade%20#{version}%20(Mac).dmg"
   appcast "https://www.minelab.com/equinox-software-update-2-0/"
