c50edb3b097debe52a34aa85a3ff739248f560c6
Peter Lewis <peter@peterlew.is>
2020-10-15 12:00:45 +0100

    Update b2.rb

--- Casks/b2.rb
+++ Casks/b2.rb
@@ -1,8 +1,8 @@
 cask "b2" do
-  version "b2-20201011-212307-c7d7c98"
+  version "20201011-212307-c7d7c98"
   sha256 "65d7f60d22145927c158feca5a60f4a0f53fc98eb727ce6394d155aa6b90cf37"
 
-  url "https://github.com/tom-seddon/b2/releases/download/#{version}/b2-osx-20201011-212307-c7d7c98.dmg"
+  url "https://github.com/tom-seddon/b2/releases/download/b2-#{version}/b2-osx-#{version}.dmg"
   appcast "https://github.com/tom-seddon/b2/releases.atom"
   name "b2"
   desc "BBC Micro emulator"
