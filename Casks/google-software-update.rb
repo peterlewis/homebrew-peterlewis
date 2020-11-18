86e4a6a53c5b30ede60a457bea855f3305686662
Peter Lewis <peter@peterlew.is>
2020-11-18 09:11:04 +0000

    Update google-software-update.rb

--- Casks/google-software-update.rb
+++ Casks/google-software-update.rb
@@ -2,7 +2,6 @@ cask "google-software-update" do
   version "1.3.15.162"
   sha256 "381c40090585f097be948059295e4c77a74e6fd6290d868d8ac5ba4538b36f3f"
 
-  # storage.googleapis.com was verified as official when first introduced to the cask
   url "https://dl.google.com/mac/install/googlesoftwareupdate.dmg"
   name "Google Software Update"
   desc "Google Software Update"
