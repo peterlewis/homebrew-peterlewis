a5ebe6aea9e11ded3c1d9fde372821a11ad87ed5
Peter Lewis <peter@peterlew.is>
2020-12-05 15:15:55 +0000

    Update jade.rb

--- Casks/jade.rb
+++ Casks/jade.rb
@@ -1,8 +1,8 @@
 cask "jade" do
   version "1.2.1"
   sha256 "b48a502f9b888e81fc463cd64137832b66bbe3a8af8fccbf18c518d6400cf5a3"
-
-  url "https://github.com/ninxsoft/JADE/releases/download/#{version}/JADE.#{version}.pkg"
+  
+  url "https://github.com/ninxsoft/JADE/releases/download/v#{version}/JADE.#{version}.pkg"
   appcast "https://github.com/ninxsoft/JADE/releases.atom"
   name "JADE"
   homepage "https://github.com/ninxsoft/JADE/"
