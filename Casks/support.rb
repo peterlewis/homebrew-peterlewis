83b4f99b080f81bf1c006c58de4ed35a3d2175b9
Peter Lewis <peter@peterlew.is>
2021-02-09 09:28:16 +0000

    Update support.rb

--- Casks/support.rb
+++ Casks/support.rb
@@ -2,7 +2,7 @@ cask "support" do
   version "2.1"
   sha256 "6d436259c3481861c90f9ead8079b50a69502a197faa089942aa4d8124058d30"
 
-  url "https://github.com/root3nl/SupportApp/releases/download/#{version}/Support.#{version}.pkg"
+  url "https://github.com/root3nl/SupportApp/releases/download/v#{version}/Support.#{version}.pkg"
   appcast "https://github.com/root3nl/SupportApp/releases.atom"
   name "Support"
   desc "Menu bar tool built for organisations"
