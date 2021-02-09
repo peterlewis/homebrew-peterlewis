a9648364d77495133280fbbda40cd7d7e3aa3609
Peter Lewis <peter@peterlew.is>
2021-02-09 09:59:28 +0000

    Update microsoft-remote-desktop.rb

--- Casks/microsoft-remote-desktop.rb
+++ Casks/microsoft-remote-desktop.rb
@@ -1,6 +1,6 @@
 cask "microsoft-remote-desktop" do
-  version "10.3.12"
-  sha256 "cf33f5fdaf0c7ac4d8d1e0a233afc513b3977f44be5cfd20784a5c3fde969ad1"
+  version "10.5.1"
+  sha256 "2b99e8980b8c57a4ab60bc2cde9b671afcdaec42e3148dc8b0df04e68b8c806f"
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Remote_Desktop_#{version}_installer.pkg"
