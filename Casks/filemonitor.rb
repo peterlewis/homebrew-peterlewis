d991e7ce18cea6e011fad9c2107c994b8fd962a7
Peter Lewis <peter@peterlew.is>
2021-02-10 11:20:19 +0000

    Update filemonitor.rb

--- Casks/filemonitor.rb
+++ Casks/filemonitor.rb
@@ -3,6 +3,7 @@ cask "filemonitor" do
   sha256 "146b837c0dcc67c038ef263724cb84becb1b4e947d10aade1ae92e1d0840e736"
 
   url "https://bitbucket.org/objective-see/deploy/downloads/FileMonitor_#{version}.zip"
+      verified: "bitbucket.org/objective-see/"
   appcast "https://objective-see.com/products/changelogs/FileMonitor.txt"
   name "FileMonitor"
   homepage "https://objective-see.com/products/utilities.html#FileMonitor"
