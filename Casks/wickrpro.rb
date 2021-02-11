e36af634a36456b7f2245c594730d934314ed957
Peter Lewis <peter@peterlew.is>
2021-02-11 13:28:45 +0000

    Update wickrpro.rb
    Added new `verified:` stanza as well as adding a `desc`

--- Casks/wickrpro.rb
+++ Casks/wickrpro.rb
@@ -3,10 +3,12 @@ cask "wickrpro" do
   sha256 "fe4bd049dce0538ce67d8494b75bf053d0ff76855488c68964f3768e227a4a21"
 
   # s3.amazonaws.com/static.wickr.com/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/pro/WickrPro-#{version}.dmg"
+  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/pro/WickrPro-#{version}.dmg",
+      verified: "s3.amazonaws.com/static.wickr.com/"
   appcast "https://pro-download.wickr.com/api/multiVerify/pro/undefined/"
   name "Wickr Pro"
   homepage "https://wickr.com/products/pro/"
+  desc "Desktop client for Wickr Pro"
 
   app "WickrPro.app"
 end
