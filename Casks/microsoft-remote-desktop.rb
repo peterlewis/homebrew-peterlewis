171e490bf5d507cf5812d5bd9dbea5a8539708ae
Peter Lewis <peter@peterlew.is>
2021-02-11 13:09:57 +0000

    Update microsoft-remote-desktop.rb URL verify
    Update to use the new `verified:` stanza

--- Casks/microsoft-remote-desktop.rb
+++ Casks/microsoft-remote-desktop.rb
@@ -2,8 +2,9 @@ cask "microsoft-remote-desktop" do
   version "10.5.1"
   sha256 "2b99e8980b8c57a4ab60bc2cde9b671afcdaec42e3148dc8b0df04e68b8c806f"
 
-  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Remote_Desktop_#{version}_installer.pkg"
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Remote_Desktop_#{version}_installer.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/"
+
   name "Microsoft Remote Desktop"
   homepage ""
 
