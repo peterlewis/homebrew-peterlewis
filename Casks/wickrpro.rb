d55b44bfe6dd7d5cca1f80cf5f559239027e1f5a
Peter Lewis <peter@peterlew.is>
2021-02-11 13:30:40 +0000

    Update wickrpro.rb
    Remove legacy verified comment

--- Casks/wickrpro.rb
+++ Casks/wickrpro.rb
@@ -2,7 +2,6 @@ cask "wickrpro" do
   version "5.72.9"
   sha256 "7651d3788ec2194d49a2343d14a4bf373c0560c56e577e357def00d5142f0b95"
 
-  # s3.amazonaws.com/static.wickr.com/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/pro/WickrPro-#{version}.dmg",
       verified: "s3.amazonaws.com/static.wickr.com/"
   appcast "https://pro-download.wickr.com/api/multiVerify/pro/undefined/"
