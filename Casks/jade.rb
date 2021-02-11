bf285ffea375f11157be7abb2938ceab37909f33
Peter Lewis <peter@peterlew.is>
2021-02-11 18:05:06 +0000

    jade style fix

--- Casks/jade.rb
+++ Casks/jade.rb
@@ -1,13 +1,13 @@
 cask "jade" do
   version "1.2.1"
   sha256 "b48a502f9b888e81fc463cd64137832b66bbe3a8af8fccbf18c518d6400cf5a3"
-  
+
   url "https://github.com/ninxsoft/JADE/releases/download/v#{version}/JADE.#{version}.pkg"
   appcast "https://github.com/ninxsoft/JADE/releases.atom"
   name "JADE"
   homepage "https://github.com/ninxsoft/JADE/"
 
   pkg "JADE.#{version}.pkg"
-  
+
   uninstall pkgutil: "com.nixsoft.pkg.JADE"
 end
