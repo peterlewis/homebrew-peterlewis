5e0166b24010a5eb7c1a0404b58120a9693bbb76
Peter Lewis <820394+peterlewis@users.noreply.github.com>
2021-03-15 12:29:42 +0000

    Update athens.rb

--- Casks/athens.rb
+++ Casks/athens.rb
@@ -1,8 +1,8 @@
 cask "athens" do
-  version "1.00-beta.55"
-  sha256 "e330be092c0f6d92494274e2694bd236ebb7e1d9e357b10728def84671ceb13b"
+  version "1.00-beta.60"
+  sha256 "f0e212f26d508dae69b2f3e5c7c55f1ca23154c8001f4ab06cbee05ea3064eb0"
 
-  url "https://athens-apps.s3.us-east-2.amazonaws.com/Athens-#{version}.dmg"
+  url "https://github.com/athensresearch/athens/releases/download/v#{version}/Athens-#{version}.dmg"
   appcast "https://github.com/athensresearch/athens/releases.atom"
   name "Athens"
   homepage "https://opencollective.com/athens"
