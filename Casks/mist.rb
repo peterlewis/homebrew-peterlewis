4c1d899aa20baa5f5eb78d51197a8768301e6153
Peter Lewis <820394+peterlewis@users.noreply.github.com>
2021-03-17 10:54:25 +0000

    Create mist.rb

---
+++ Casks/mist.rb
@@ -0,0 +1,13 @@
+cask "mist" do
+  version "1.1"
+  sha256 "0ea600ef66f2276287567cf680264a030fbb5a331df828257e71bbee10e35f25"
+
+  url "https://github.com/ninxsoft/Mist/releases/download/v#{version}/Mist.#{version}.pkg"
+  appcast "https://github.com/ninxsoft/Mist/releases.atom"
+  name "JADE"
+  homepage "https://github.com/ninxsoft/Mist/"
+
+  pkg "Mist.#{version}.pkg"
+
+  uninstall pkgutil: "com.nixsoft.pkg.mist"
+end
