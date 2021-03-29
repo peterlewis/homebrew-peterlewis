0c387c0fa74952ecb2fc3ca5f957b801243e5dcf
Peter Lewis <820394+peterlewis@users.noreply.github.com>
2021-03-29 10:59:01 +0100

    Update get-iplayer from 3.26.1 to 3.27.0

--- Casks/get-iplayer.rb
+++ Casks/get-iplayer.rb
@@ -1,6 +1,6 @@
 cask "get-iplayer" do
-  version "3.26.1"
-  sha256 "27ba02140ce69da68e0ca022e02486d9fc88af3d217b8cdb9e618104a8b493d9"
+  version "3.27.0"
+  sha256 "8bf559751b5073e0e44d2fbc39e4f8281baf37cd2f238465ada3b4a81a9e56b5"
 
   url "https://github.com/get-iplayer/get_iplayer_macos/releases/download/#{version}/get_iplayer-#{version}-macos-x64.pkg"
   appcast "https://github.com/get-iplayer/get_iplayer_macos/releases.atom"
