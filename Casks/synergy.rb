dacd4660d50fd27d76456680e4d1b9f2bc7880b8
Peter Lewis <peter@peterlew.is>
2025-06-01 17:10:13 +0100

    Create synergy.rb

---
+++ Casks/synergy.rb
@@ -0,0 +1,11 @@
+cask "synergy" do
+  version "3.3.1"
+  sha256 "1aa7e92182623d4bba7da48c98c27e67f5aaa15bb8481c6c963fb6990ba77eae"
+
+  url "https://symless.com/synergy/download/package/macos-12.0/synergy-#{version}-macos-arm64.dmg"
+  appcast "https://symless.com/synergy/download"
+  name "Minelab Update Utility"
+  homepage "https://symless.com/synergy/"
+
+  app "Synergy.app"
+end
