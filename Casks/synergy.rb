e0eb168a46726155e578a3abbc9c4d62b1c4d080
Peter Lewis <peter@peterlew.is>
2025-06-01 17:41:43 +0100

    Update synergy.rb

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -3,7 +3,6 @@ cask "synergy" do
   sha256 "1aa7e92182623d4bba7da48c98c27e67f5aaa15bb8481c6c963fb6990ba77eae"
 
   url "https://d3mfpejpd1vtyz.cloudfront.net/package/synergy-#{version}-macos-arm64.dmg?Expires=1748796300&Key-Pair-Id=K2BPMREYBMCTR6&Signature=UPVbOYvaZ1M2jQApJik2NoMe--f76nf-mpxtIzlsEKWY8Hv6Rgb6h~KyzmTUxAaks-w8sX9ysIxNO1leegNY55rSHWvNDg4-tEkWPdX2A1Oasz7LEmvpD9STOdg4xPw4ijstYtxkK-K1L7HW1uLIqsF-8fbRvnsGvLO5-CSyQFfVRR9tKWYuhuLr2yqaYADSI2iP8YWtxK6zSlNQu7EBDiYFPospsq-1gpPOl1hkqcc9JPAmjUdLSz8CpupjPGz9LEJp0k70YirYVk~FNZBr45kMchUHZUJWGn2usCMfJXLUXIlS~xCmrSWe2HpEJID8w~d5YPMB-BMgx5FosdbhoQ__"
-  appcast "https://symless.com/synergy/download"
   name "Synergy"
   homepage "https://symless.com/synergy/"
 
